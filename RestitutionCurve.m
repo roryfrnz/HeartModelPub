b84a7eeb4fbb7b1db2b5bc51e5941a9bbd5f30a9
Rory Fenwick-Rose <rfen801@aucklanduni.ac.nz>
2026-01-23 13:24:54 +1300

    Fix RestitutionCurve plotting

--- RestitutionCurve.m
+++ RestitutionCurve.m
@@ -1,11 +1,11 @@
 
 
-% Find the action potential durations from simulation data
-[Durations_temp, DI_temp] = APD(out.ScopeData{1}.Values.Data, out.ScopeData{1}.Values.Time);
-[Durations_temp1, DI_temp1] = APD(out.ScopeData1{1}.Values.Data, out.ScopeData1{1}.Values.Time);
-[Durations_temp2, DI_temp2] = APD(out.ScopeData2{1}.Values.Data, out.ScopeData2{1}.Values.Time);
-
-BCL = PulsePeriod;
+% % Find the action potential durations from simulation data
+% [Durations_temp, DI_temp] = APD(out.ScopeData{1}.Values.Data, out.ScopeData{1}.Values.Time);
+% [Durations_temp1, DI_temp1] = APD(out.ScopeData1{1}.Values.Data, out.ScopeData1{1}.Values.Time);
+% [Durations_temp2, DI_temp2] = APD(out.ScopeData2{1}.Values.Data, out.ScopeData2{1}.Values.Time);
+% 
+% BCL = PulsePeriod;
 
 if isfile("Durations_Paced.mat")
     load("Durations_Paced.mat");
@@ -23,20 +23,20 @@ else
     DI2 = [];
 end
 
-Durations_temp = horzcat(Durations_temp, BCL*ones(length(Durations_temp),1));
-Durations_temp1 = horzcat(Durations_temp1, BCL*ones(length(Durations_temp1),1));
-Durations_temp2 = horzcat(Durations_temp2, BCL*ones(length(Durations_temp2),1));
-
-Durations = vertcat(Durations, Durations_temp);
-Durations1 = vertcat(Durations1, Durations_temp1);
-Durations2 = vertcat(Durations2, Durations_temp2);
-
-DI = vertcat(DI, DI_temp);
-DI1 = vertcat(DI1, DI_temp1);
-DI2 = vertcat(DI2, DI_temp2);
-
-save("Durations_Paced.mat", "Durations", "Durations1", "Durations2");
-save("DI_Paced.mat", "DI", "DI1", "DI2");
+% Durations_temp = horzcat(Durations_temp, BCL*ones(length(Durations_temp),1));
+% Durations_temp1 = horzcat(Durations_temp1, BCL*ones(length(Durations_temp1),1));
+% Durations_temp2 = horzcat(Durations_temp2, BCL*ones(length(Durations_temp2),1));
+% 
+% Durations = vertcat(Durations, Durations_temp);
+% Durations1 = vertcat(Durations1, Durations_temp1);
+% Durations2 = vertcat(Durations2, Durations_temp2);
+% 
+% DI = vertcat(DI, DI_temp);
+% DI1 = vertcat(DI1, DI_temp1);
+% DI2 = vertcat(DI2, DI_temp2);
+% 
+% save("Durations_Paced.mat", "Durations", "Durations1", "Durations2");
+% save("DI_Paced.mat", "DI", "DI1", "DI2");
 
 % Plot Durations against DI to find the Restitution Curve
 figure
@@ -50,7 +50,7 @@ scatter1 = scatter(DI, Durations(:,1), 50, cmap(round(Durations(:,2)*301.1765), 
 scatter2 = scatter(DI1, Durations1(:,1), 50, cmap(round(Durations1(:,2)*301.1765), :), "square", 'DisplayName', 'Node 2 (M)');
 scatter3 = scatter(DI2, Durations2(:,1), 50, cmap(round(Durations2(:,2)*301.1765), :), "^", 'DisplayName', 'Node 3 (M)');
 
-c = colorbar('TickLabels',{'0','0.085','0.17','0.255','0.34','0.425','0.51','0.595','0.68','0.765','0.85'});;
+c = colorbar('TickLabels',{'0','0.085','0.17','0.255','0.34','0.425','0.51','0.595','0.68','0.765','0.85'});
 c.Label.String = 'Base Cycle Length (s)';
 
 scatter1.DataTipTemplate.DataTipRows(1).Label = 'Diastolic Interval (s)';
@@ -76,4 +76,4 @@ title('Restitution Curve')
 ylabel('Action Potential Duration (s)');
 xlabel('Diastolic Interval (s)');
 
-APPlot
+% APPlot
