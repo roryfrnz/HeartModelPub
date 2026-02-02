f70838540ebe7bd347311cb009de30f2eb6fe58c
Rory Fenwick-Rose <rfen801@aucklanduni.ac.nz>
2026-02-02 15:23:40 +1300

    Update APPlot for 4 Node Example

--- APPlot.m
+++ APPlot.m
@@ -1,24 +1,27 @@
 % Assign scope data from each node to a variable
 Node1AP = out.ScopeData{1}.Values.Data;
-Node2AP = out.ScopeData1{1}.Values.Data;
+Node2AP = out.ScopeData3{1}.Values.Data;
 Node3AP = out.ScopeData2{1}.Values.Data;
+Node4AP = out.ScopeData1{1}.Values.Data;
 
 time = out.ScopeData{1}.Values.Time;
 
 [Durations1, DI1, locs1, APD90Times1, APD90Values1, pks1] = APD(Node1AP, time);
 [Durations2, DI2, locs2, APD90Times2, APD90Values2, pks2] = APD(Node2AP, time);
 [Durations3, DI3, locs3, APD90Times3, APD90Values3, pks3] = APD(Node3AP, time);
+[Durations4, DI4, locs4, APD90Times4, APD90Values4, pks4] = APD(Node4AP, time);
 
 pks1 = pks1*0.5;
 pks2 = pks2*0.5;
 pks3 = pks3*0.5;
+pks4 = pks4*0.5;
 
 % Plot AP data against time
 figure
 
-ax(1) = subplot(3, 1, 1);
+ax(1) = subplot(4, 1, 1);
 plot(time, Node1AP, "black");
-title('Node 1 (NM)');
+title('Cell 1 (NM)');
 ylabel('Voltage (mV)');
 xlabel('Time (s)');
 
@@ -38,9 +41,9 @@ plot([APD90Times1 APD90Times1], [-300 15], ':r', 'DisplayName', 'APD90');
 
 hold off
 
-ax(2) = subplot(3, 1, 2);
+ax(2) = subplot(4, 1, 2);
 plot(time, Node2AP, "black");
-title('Node 2 (M)');
+title('Fast Cell (NM)');
 ylabel('Voltage (mV)');
 xlabel('Time (s)');
 
@@ -56,9 +59,9 @@ plot([APD90Times1 APD90Times1], yl, ':r');
 
 hold off
 
-ax(3) = subplot(3, 1, 3);
+ax(3) = subplot(4, 1, 3);
 plot(time, Node3AP, "black");
-title('Node 3 (M)');
+title('Slow Cell (NM)');
 ylabel('Voltage (mV)');
 xlabel('Time (s)');
 
@@ -69,6 +72,24 @@ hold on
 
 scatter(locs3, pks3, "black");
 scatter(APD90Times3, APD90Values3, "black");
+plot([locs1 locs1], yl, '--r');
+plot([APD90Times1 APD90Times1], yl, ':r');
+
+hold off
+
+ax(3) = subplot(4, 1, 4);
+plot(time, Node4AP, "black");
+title('Cell 2 (NM)');
+ylabel('Voltage (mV)');
+xlabel('Time (s)');
+
+yl = ylim;
+ylim(yl);
+
+hold on
+
+scatter(locs4, pks4, "black");
+scatter(APD90Times4, APD90Values4, "black");
 plot([locs1 locs1], yl, '--r');
 plot([APD90Times1 APD90Times1], yl, ':r');
 
