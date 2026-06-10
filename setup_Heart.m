c67451b34a9d82ba6f361982d3105b9a1682f0a3
Rory Fenwick-Rose <rfen801@aucklanduni.ac.nz>
2026-06-11 11:16:26 +1200

    add dashboard to tune parameters in real time to example 1

--- setup_Heart.m
+++ setup_Heart.m
@@ -8,4 +8,4 @@ addpath([path_var,filesep 'src']);
 addpath([path_var,filesep 'models']);
 addpath([path_var,filesep 'models_refine']);
 addpath([path_var,filesep 'Examples']);
-addpath([path_var,filesep 'Examples/models']);
+addpath([path_var,filesep 'Examples\models']);
